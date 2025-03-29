d572271e95c954c0bd5c7c4279eb62ed85c68619
Andy Hislop <andrewhislop2@gmail.com>
2025-03-29 23:04:01 +0000

    First commit

--- App/__init__.py
+++ App/__init__.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, current_app
 from flask_sqlalchemy import SQLAlchemy
-from os import path, makedirs
+from os import path, makedirs, environ
 from flask_login import login_manager
 from werkzeug.security import generate_password_hash
 
@@ -10,9 +10,21 @@ DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'a'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{path.abspath(path.join(path.dirname(__file__), DB_NAME))}'
+    app.config['SECRET_KEY'] = environ.get('SECRET_KEY', 'fallback')
+    database_uri = environ.get('DATABASE_URL', f'sqlite:///{DB_NAME}')
+    if database_uri.startswith('postgres://'):
+        database_uri = database_uri.replace('postgres://', 'postgresql://', 1)
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
+    print(f"SQLALCHEMY_DATABASE_URI: {app.config['SQLALCHEMY_DATABASE_URI']}")
     db.init_app(app)
+
+    from .models import User, Match
+
+    if database_uri.startswith('sqlite://'):
+        create_database(app)
+    else:
+        with app.app_context():
+            db.create_all()
 
     from .views import views
     from .auth import auth
@@ -20,30 +32,29 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    from .models import User, Match
-
-    with app.app_context():
-        db.create_all()
-
     manager = login_manager.LoginManager()
     manager.login_view = 'auth.login'
     manager.init_app(app)
 
-    create_database(app)
-
     @manager.user_loader
     def load_user(id):
+        from .models import User
         return User.query.get(int(id))
 
     return app
 
+
 def create_database(app):
     db_path = path.abspath(path.join(path.dirname(__file__), DB_NAME))
+    # db_path = "C:/Users/andrew.hislop/PycharmProjects/OfficELO/App/test_database.db"
+    print(f"Database path: {db_path}")
     if not path.exists(db_path):
         try:
             makedirs(path.dirname(db_path), exist_ok=True)
             with app.app_context():
+                from .models import User, Match
                 db.create_all()
+                print("Database created successfully!")
         except Exception as e:
-            print(f"Error creating database: {e}")
+            current_app.logger.error(f"Error creating database: {e}")
 
